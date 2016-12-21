23172b721b690667994f91b936f5033a5b713d57
sepiro2000 <sepiro2000@sepiro2000ui-MacBook-Pro.local>
2016-12-21 10:13:01 +0900

    fix db connection bug

--- src/main/java/com/kakao/infra/JavaThreadDumpAnalyzerApplication.java
+++ src/main/java/com/kakao/infra/JavaThreadDumpAnalyzerApplication.java
@@ -2,8 +2,10 @@ package com.kakao.infra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class JavaThreadDumpAnalyzerApplication {
 	
 
